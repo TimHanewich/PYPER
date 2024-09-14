fa48cfc56ad6d380a8422057d66b00dae870caed
Tim Hanewich <tahanewich@live.com>
2024-09-14 13:47:15 -0400

    Reduced output power

--- derivatives/PYPER LoRa/src/main.py
+++ derivatives/PYPER LoRa/src/main.py
@@ -54,7 +54,7 @@ try:
     tools.log("Configuring LoRa...")
     lora.networkid = 18
     lora.address = 1 # 0 = controller, 1 = rover
-    lora.output_power = 22
+    lora.output_power = 12
     lora.band = 960000000 # set band to highest (fastest)
     lora.rf_parameters = (7, 9, 1, 8) # Spreadig Factor of 7, Bandwidth of 500 KHz, Coding Rate of 1, Programmed Preamble of 8
     tools.log("LoRa configured!")
