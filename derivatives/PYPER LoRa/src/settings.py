a2ac6fd2a5fe427ba8633725a36ac2080f453b27
Tim Hanewich <tahanewich@live.com>
2024-09-14 13:45:22 -0400

    dlog off

--- derivatives/PYPER LoRa/src/settings.py
+++ derivatives/PYPER LoRa/src/settings.py
@@ -14,4 +14,6 @@ gp_lora_tx:int = 16 # the pin the raspberry pi will use to SEND data to the RYLR
 gp_battery:int = 26 # the analog-to-digital pin that will be used to sense the battery state of charge (using a voltage divider)
 
 # diagnostic logging
-dlog:bool = True
+# if flipped to true, all printing will be logged to a file, "log.txt". 
+# This will significantly slow down operations but is good for debugging.
+dlog:bool = False 
