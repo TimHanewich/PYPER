363a8d5dd05a6607234d768c3c11d663808f797c
Tim Hanewich <tahanewich@live.com>
2024-09-25 19:31:36 -0400

    Added optionnal developer message

--- derivatives/PYPER LoRa/src/tools.py
+++ derivatives/PYPER LoRa/src/tools.py
@@ -2,9 +2,12 @@ import time
 import sys
 import settings
 
-def log_exc(exc:Exception) -> None:
+def log_exc(exc:Exception, msg:str = None) -> None:
     f = open("log.txt", "a")
     f.write("--- EXCEPTION at " + str(time.ticks_ms()) + " ms ---" + "\n")
+    if msg != None: # if they provided an optional msg, append it after
+        f.write("Developer Msg: " + msg)
+        f.write("\n")
     sys.print_exception(exc, f)
     f.write("\n")
     f.close()
